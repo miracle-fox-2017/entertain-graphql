7c06b60685458bdbd47a308425361ed359599646
ameliarahman <amel.rahman5@gmail.com>
2018-01-09 06:31:27 +0700

    get data from graphql

--- client/App.js
+++ client/App.js
@@ -1,21 +1,26 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import ProfileList from './components/ProfileList'
+import ProfileList from './components/ProfileList';
+import { ApolloClient } from 'apollo-client';
+import { HttpLink } from 'apollo-link-http';
+import { InMemoryCache } from 'apollo-cache-inmemory';
 import {
-  ApolloClient,
-  gql,
-  graphql,
   ApolloProvider
 } from 'react-apollo'
 
-const client = new ApolloClient()
+const client = new ApolloClient({
+  link: new HttpLink({ uri: 'http://192.168.1.101:3000/graphql' }),
+  cache: new InMemoryCache()
+})
 
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <ProfileList />
-      </View>
+      <ApolloProvider client={client}>
+        <View style={styles.container}>
+          <ProfileList />
+        </View>
+      </ApolloProvider>
     );
   }
 }
